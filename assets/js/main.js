559b99267049410ea292d1d86d29d3585f0491c9
angelacaterina <angela.caterina@icloud.com>
2021-01-11 16:04:26 +0100

    vue-dischi

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	//Code
+  // Servendoci di Vue JS stampiamo tutto a schermo.
+  let app = new Vue({
+    el: '#app',
+    data: {
+      albumList:[],
+    },
+    mounted(){
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music') // Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+      .then(resp => {
+        console.log(resp.data.response);
+        this.albumList = resp.data.response;
+      })
+    }
+  })
+});
+
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
