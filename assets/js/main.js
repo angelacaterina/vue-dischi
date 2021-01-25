d9202b367150a004fd74d59bd9865df096eb55fd
angelacaterina <angela.caterina@icloud.com>
2021-01-25 15:23:15 +0100

    refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     el: '#app',
     data: {
       albumList:[],
+      // searchGender: '',
     },
     mounted(){
       axios.get('https://flynn.boolean.careers/exercises/api/array/music') // Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
@@ -12,8 +13,14 @@ $(document).ready(function() {
         console.log(resp.data.response);
         this.albumList = resp.data.response;
       })
-    }
+    },
+    // // Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+    // computed: {
+    //   filteredSearch(){
+    //     return this.albumList.filter(album => {
+    //       return album.genre.includes(this.searchGender)
+    //     })
+    //   }
+    // }
   })
 });
-
-// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
